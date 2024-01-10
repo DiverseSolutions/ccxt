ff1e095c58800ee5e043df048353c13e6ecb794c
bbattulga <battulga.dev@gmail.com>
2024-01-10 12:41:16 +0800

    update trademn

--- js/trademn.js
+++ js/trademn.js
@@ -134,7 +134,7 @@ module.exports = class trademn extends Exchange {
         if (since === undefined) {
             request['start'] = parseInt (this.milliseconds() / 1000 - 48 * 60 * 60);
         } else {
-            request['start'] = since;
+            request['start'] = parseInt (since / 1000);
         }
         if (limit === undefined) {
             request['end'] = parseInt (this.milliseconds() / 1000);
